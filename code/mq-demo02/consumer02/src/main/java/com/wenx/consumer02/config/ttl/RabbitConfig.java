8628e92b60011229e0e55ba7c9b64f34f4f8bf43
wsnkf <2182908911@qq.com>
2022-08-28 11:29:50 +0800

    ttl queue code

--- code/mq-demo02/consumer02/src/main/java/com/wenx/consumer02/config/ttl/RabbitConfig.java
+++ code/mq-demo02/consumer02/src/main/java/com/wenx/consumer02/config/ttl/RabbitConfig.java
@@ -7,10 +7,15 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RabbitConfig {
     public static final String JAVABOY_MESSAGE_DEALY_QUEUE_NAME = "javaboy_message_delay_queue_name";
     public static final String JAVABOY_MESSAGE_DELAY_EXCHANGE_NAME = "javaboy_message_delay_exchange_name";
+
+    public static final String JAVABOY_QUEUE_DEMO = "JAVABOY_QUEUE_DEMO";
 
 
     @Bean
@@ -20,13 +25,20 @@ public class RabbitConfig {
 
     @Bean
     DirectExchange messageDirectExchange(){
-        return new DirectExchange(JAVABOY_MESSAGE_DELAY_EXCHANGE_NAME,true,false);
+        return new DirectExchange(JAVABOY_QUEUE_DEMO,true,false);
+    }
+
+    @Bean
+    Queue queue() {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-message-ttl", 10000);
+        return new Queue(JAVABOY_QUEUE_DEMO, true, false, false, args);
     }
 
     @Bean
     Binding messageDelayBinding(){
-        return BindingBuilder.bind(messageQueue())
+        return BindingBuilder.bind(queue())
                 .to(messageDirectExchange())
-                .with(JAVABOY_MESSAGE_DEALY_QUEUE_NAME);
+                .with(JAVABOY_QUEUE_DEMO);
     }
 }
--- code/mq-demo02/consumer02/src/main/java/com/wenx/consumer02/controller/HelloController.java
+++ code/mq-demo02/consumer02/src/main/java/com/wenx/consumer02/controller/HelloController.java
@@ -8,6 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//@RestController
+//public class HelloController {
+//
+//    @Autowired
+//    RabbitTemplate rabbitTemplate;
+//
+//    @GetMapping("/send")
+//    public void hello(){
+//        Message build = MessageBuilder.withBody("hello javaboy".getBytes())
+//                // 设置过期时间 10s 消费达到 RabbitMQ 10s 内还没有人消费，消息就会过期
+//                .setExpiration("10000")
+//                .build();
+//
+//        rabbitTemplate.send(RabbitConfig.JAVABOY_MESSAGE_DELAY_EXCHANGE_NAME,RabbitConfig.JAVABOY_MESSAGE_DEALY_QUEUE_NAME, build);
+//    }
+//}
+
 @RestController
 public class HelloController {
 
@@ -16,11 +33,6 @@ public class HelloController {
 
     @GetMapping("/send")
     public void hello(){
-        Message build = MessageBuilder.withBody("hello javaboy".getBytes())
-                // 设置过期时间 10s 消费达到 RabbitMQ 10s 内还没有人消费，消息就会过期
-                .setExpiration("10000")
-                .build();
-
-        rabbitTemplate.send(RabbitConfig.JAVABOY_MESSAGE_DELAY_EXCHANGE_NAME,RabbitConfig.JAVABOY_MESSAGE_DEALY_QUEUE_NAME, build);
+        rabbitTemplate.convertAndSend(RabbitConfig.JAVABOY_MESSAGE_DELAY_EXCHANGE_NAME, RabbitConfig.JAVABOY_QUEUE_DEMO, "hello");
     }
 }
